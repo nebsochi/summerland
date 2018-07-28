aecc6dd7bcd5094714ad6746a40152470828df4a
nebsochi <nebechi@2ilabs.co>
2018-07-28 16:00:04 +0100

    hosted  DB added

--- includes/database.php
+++ includes/database.php
@@ -1,4 +1,4 @@
 <?php
 //$conn = mysqli_connect("localhost","root","","realestate");
 //$conn = mysqli_connect("localhost","root","","chibuzor");
-$conn = mysqli_connect("db4free.net","nebechi","nebsochi","realestate");
+$conn = mysqli_connect("db4free.net","nebsocgihi","nebsochi","realestate");
